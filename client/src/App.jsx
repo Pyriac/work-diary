40fdc786a6a4c5bd171544a3dedd9770f895cc5a
Pyriac <cyriacpineau@gmail.com>
2024-09-05 15:40:08 +0200

    :art: placing css in different document for lisibility

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import { Outlet } from "react-router-dom";
 import Nav_bar from "./components/Nav_bar";
 
 import "./assets/styles/app.css";
+import "./assets/styles/home.css";
+import "./assets/styles/card.css";
 
 function App() {
   return (
