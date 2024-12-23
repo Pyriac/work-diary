a4676c39726c6ed933281ee145595b9b4c50683c
Pyriac <cyriacpineau@gmail.com>
2024-12-23 13:36:30 +0100

    affichage selon archive ou non

--- client/src/pages/Tasks.jsx
+++ client/src/pages/Tasks.jsx
@@ -1,18 +1,26 @@
 import { Link, useLoaderData } from "react-router-dom";
 import "../assets/styles/app.css";
 import Detail_tasks from "../components/Detail_Tasks";
+import { useEffect, useState } from "react";
 
 export default function Tasks() {
+  const [archiveFilter, setArchiveFilter] =useState(0)
   const data = useLoaderData();
 
-  const sortedData = data.sort((a, b) => {
+  let sortedData = data.sort((a, b) => {
     return new Date(a.deadline) - new Date(b.deadline);
   });
 
+
+
   return (
+   
     <>
+       <button onClick={() => setArchiveFilter((prev) => (prev === 0 ? 1 : 0))}>
+        {archiveFilter === 1 ? 'Afficher les archivés' : 'Afficher les actifs'}
+      </button>
       <div className="all_tasks">
-        {sortedData.map((task) => (
+        {sortedData.filter((task) => task.is_active === archiveFilter).map((task) => (
           <Link to={`/task/${task.id}`} key={task.id}>
             <Detail_tasks data={task} />
           </Link>
